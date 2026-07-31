3fd25bd4f53fdf272a71e5558feba7e7de916a47
tito doni <gitarkopong@Gmail.com>
2026-07-31 08:14:35 +0700

    feat: redirect pogrid.id app routes to app.pogrid.id using RedirectToAppDomain middleware

---
+++ app/Http/Middleware/RedirectToAppDomain.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectToAppDomain
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $host = $request->getHost();
+
+        // Enforce only for pogrid.id domain in production
+        if (str_ends_with($host, 'pogrid.id')) {
+            $isAppSubdomain = str_starts_with($host, 'app.');
+
+            if (!$isAppSubdomain && $request->path() !== '/') {
+                // If on main domain (pogrid.id) and requesting any route except '/',
+                // redirect them to the same route on app.pogrid.id
+                return redirect()->secure('https://app.pogrid.id/' . ltrim($request->getRequestUri(), '/'));
+            }
+        }
+
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\HandleInertiaRequests;
 use App\Http\Middleware\SetTenant;
+use App\Http\Middleware\RedirectToAppDomain;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -19,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
+            RedirectToAppDomain::class,
             HandleInertiaRequests::class,
             SetTenant::class,
         ]);
